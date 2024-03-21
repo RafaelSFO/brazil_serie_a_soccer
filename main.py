51124e72d882a1c05b2a4a46773973c3b214a004
Rafael <rafael.samick@outlook.com>
2024-03-20 21:54:34 -0300

    Added code to get data from all seasons

--- main.py
+++ main.py
@@ -2,51 +2,91 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import numpy as np
+# %%
+# Defining the seasons
+seasons = ['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023']
+global all_seasons
+all_seasons = pd.DataFrame()
 
-# Getting content of the page
-html_page = requests.get('https://fbref.com/en/comps/24/2022/2022-Serie-A-Stats')
+data_list_full = []
+def get_season_table(year):
+    # Getting content of the page
+    url_page = f'https://fbref.com/en/comps/24/{year}/{year}-Serie-A-Stats'
+    html_page = requests.get(url_page)
+    if html_page.status_code == 200:
+        print('Conection ok!')
+    else:
+        print('No conection')
+    # Parsing HTML page with BeautifulSoup to get the table
+    page_with_bs = BeautifulSoup(html_page.content, 'html5lib')
+    table_id = f'results{year}241_overall'
+    table = page_with_bs.find('table', {'id': table_id})
+    #Getting the wanted columns
+    columns = []
+    for column in table.find_all('th')[0:19]:
+        columns.append(column.text.strip().replace('/', '').replace(' ', '_'))
+    # Inside each 'tr' is where whe can find the content of a row 'td'
+    for each_row in table.tbody.find_all('tr'):
+        rows = each_row.find_all('td')
+        rnk_rows = each_row.find_all('th')
+        rnk = rnk_rows[0].text.strip()
+        squad = rows[0].text.strip()
+        mp = rows[1].text.strip()
+        w = rows[2].text.strip()
+        d = rows[3].text.strip()
+        l = rows[4].text.strip()
+        gf = rows[5].text.strip()
+        ga = rows[6].text.strip()
+        gd = rows[7].text.strip()
+        pts = rows[8].text.strip()
+        pts_mp = rows[9].text.strip()
+        if len(rows) == 18:
+            xg = rows[10].text.strip()
+            xga = rows[11].text.strip()
+            xgd = rows[12].text.strip()
+            xgd_90 = rows[13].text.strip()
+            attendance = rows[14].text.strip()
+            top_team_scorer = rows[15].text.strip()
+            goalkeeper = rows[16].text.strip()
+            notes = rows[17].text.strip()
+        else:
+            xg = np.nan
+            xga = np.nan
+            xgd = np.nan
+            xgd_90 = np.nan
+            attendance = rows[10].text.strip()
+            top_team_scorer = rows[11].text.strip()
+            goalkeeper = rows[12].text.strip()
+            notes = rows[13].text.strip()
+        data_list = [rnk, squad, mp, w, d, l, gf, ga, gd, pts, pts_mp, xg, xga, xgd, xgd_90, attendance, top_team_scorer, goalkeeper, notes, year]
+        data_list_full.append(data_list)
+    data_list_full.insert(0, columns)
+    return data_list_full
 # %%
-# Checking if the conection with the website is working or not
-if html_page.status_code == 200:
-    print('Ok!')
-else:
-    print('No conection')
+for year in seasons:
+    data_from_list = get_season_table(year)
+del data_from_list[0]
 # %%
-# Parsing HTML page with BeautifulSoup to get the table
-page_with_bs = BeautifulSoup(html_page.content, 'html5lib')
-table = page_with_bs.find('table', {'id': 'results2022241_overall'})
+pd.DataFrame(columns=['Rk',
+ 'Squad',
+ 'MP',
+ 'W',
+ 'D',
+ 'L',
+ 'GF',
+ 'GA',
+ 'GD',
+ 'Pts',
+ 'PtsMP',
+ 'xG',
+ 'xGA',
+ 'xGD',
+ 'xGD90',
+ 'Attendance',
+ 'Top_Team_Scorer',
+ 'Goalkeeper',
+ 'Notes', 'Year'], data=data_from_list).to_excel(r'C:\Users\rafae\Documents\RAFAEL - ESTUDOS\Data Engineering\teste.xlsx', index=False)
 # %%
-# Getting the wanted columns
-columns = []
-for column in table.find_all('th')[0:19]:
-    columns.append(column.text.strip().replace('/', '').replace(' ', '_'))
+
 # %%
-regular_season = pd.DataFrame()
-index_num = 0
-# Inside each 'tr' is where whe can find the content of a row 'td'
-for each_row in table.tbody.find_all('tr'):
-    rows = each_row.find_all('td')
-    rnk_rows = each_row.find_all('th')
-    rnk = rnk_rows[0].text.strip()
-    squad = rows[0].text.strip()
-    mp = rows[1].text.strip()
-    w = rows[2].text.strip()
-    d = rows[3].text.strip()
-    l = rows[4].text.strip()
-    gf = rows[5].text.strip()
-    ga = rows[6].text.strip()
-    gd = rows[7].text.strip()
-    pts = rows[8].text.strip()
-    pts_mp = rows[9].text.strip()
-    xg = rows[10].text.strip()
-    xga = rows[11].text.strip()
-    xgd = rows[12].text.strip()
-    xgd_90 = rows[13].text.strip()
-    attendance = rows[14].text.strip()
-    top_team_scorer = rows[15].text.strip()
-    goalkeeper = rows[16].text.strip()
-    notes = rows[17].text.strip()
-    regular_season = pd.concat([regular_season, pd.DataFrame.from_records({'Rk': rnk, 'Squad':squad, 'MP': mp,
-    'W': w, 'D': d, 'L': l, 'GF': gf, 'GA': ga, 'GD': gd, 'Pts': pts, 'PtsMP': pts_mp,
-    'xG': xg, 'xGA':xga, 'xGD':xgd, 'xGD90': xgd_90, 'Attendance': attendance, 'Top_Team_Scorer': top_team_scorer, 'Goalkeeper': goalkeeper, 'Notes':notes}, columns=columns, index=[index_num])], axis=0)
-    index_num = index_num + 1
