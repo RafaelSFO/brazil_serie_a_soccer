e7d17948c90c2ee1deac6fc990cde9ddf6429be6
Rafael <rafael.samick@outlook.com>
2024-03-21 21:10:46 -0300

    Got the main table from all the seasons and uploaded it to bigquery

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # %%
 from bs4 import BeautifulSoup
 import pandas as pd
-import requests
 import numpy as np
+import requests
 # %%
 # Defining the seasons
 seasons = ['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023']
-global all_seasons
-all_seasons = pd.DataFrame()
-
 data_list_full = []
-def get_season_table(year):
+
+def get_main_season_table(year):
     # Getting content of the page
     url_page = f'https://fbref.com/en/comps/24/{year}/{year}-Serie-A-Stats'
     html_page = requests.get(url_page)
@@ -22,7 +20,7 @@ def get_season_table(year):
     page_with_bs = BeautifulSoup(html_page.content, 'html5lib')
     table_id = f'results{year}241_overall'
     table = page_with_bs.find('table', {'id': table_id})
-    #Getting the wanted columns
+    # Getting the wanted columns
     columns = []
     for column in table.find_all('th')[0:19]:
         columns.append(column.text.strip().replace('/', '').replace(' ', '_'))
@@ -61,32 +59,41 @@ def get_season_table(year):
             notes = rows[13].text.strip()
         data_list = [rnk, squad, mp, w, d, l, gf, ga, gd, pts, pts_mp, xg, xga, xgd, xgd_90, attendance, top_team_scorer, goalkeeper, notes, year]
         data_list_full.append(data_list)
-    data_list_full.insert(0, columns)
-    return data_list_full
+    return columns, data_list_full
 # %%
 for year in seasons:
-    data_from_list = get_season_table(year)
-del data_from_list[0]
+    columns, data_from_list = get_main_season_table(year)
 # %%
-pd.DataFrame(columns=['Rk',
- 'Squad',
- 'MP',
- 'W',
- 'D',
- 'L',
- 'GF',
- 'GA',
- 'GD',
- 'Pts',
- 'PtsMP',
- 'xG',
- 'xGA',
- 'xGD',
- 'xGD90',
- 'Attendance',
- 'Top_Team_Scorer',
- 'Goalkeeper',
- 'Notes', 'Year'], data=data_from_list).to_excel(r'C:\Users\rafae\Documents\RAFAEL - ESTUDOS\Data Engineering\teste.xlsx', index=False)
+# Insertin the string 'Year' to be the last column
+columns.insert(len(columns), 'Year')
+df_all_seasons = pd.DataFrame(columns=columns, data=data_from_list)
+# Getting information about the types from the dataframe
+df_all_seasons.info()
+# Getting just the name of the top scorer
+df_all_seasons['Top_Team_Scorer_Name'] = df_all_seasons['Top_Team_Scorer'].str.split('-').str[0].str.strip()
+# Getting Goals from the top scorer
+df_all_seasons['Scorer_Goals'] = df_all_seasons['Top_Team_Scorer'].str.split('-').str[-1].str.strip()
+# Transforming data types
+to_int = ['Rk', 'MP', 'W', 'D', 'L', 'GF', 'Pts', 'Scorer_Goals']
+df_all_seasons[to_int] = df_all_seasons[to_int].astype('int')
+to_float = ['PtsMP', 'xG', 'xGA', 'xGD', 'xGD90']
+df_all_seasons[to_float] = df_all_seasons[to_float].astype('float')
+# This columns will no longer be used 
+df_all_seasons.drop(columns='Top_Team_Scorer', inplace=True)
+# Reordering columns
+df_all_seasons = df_all_seasons[['Rk', 'Squad', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts', 'PtsMP','xG', 'xGA', 'xGD', 
+       'xGD90', 'Attendance', 'Top_Team_Scorer_Name', 'Scorer_Goals', 'Goalkeeper', 'Notes','Year']]
 # %%
+# Uploading our data to the Analytics tool from Google Cloud Plataform, BigQuery
+df_all_seasons.to_gbq(
+    destination_table ='history.all_seasons',
+    project_id = 'brazilian-serie-a',
+    if_exists= 'replace'
+)
+# %%
+# Questions to reply
 
-# %%
+# 1 - top scorers from each season
+# 2 - Team with the most win ever
+# 3 - Team with the most defeats
+# 4 - Team with more qualifications to Libertadores
